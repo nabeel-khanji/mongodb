54df5c88876007f9f189ec21e0e4190c3122e5bc
Muhammad Nabeel <muhammad.nabeel@cubixlabs.com>
2022-05-06 18:58:26 +0500

    mongoose multi docs insert

---
+++ multi_doc_mongoose/src/app.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+mongoose
+  .connect("mongodb://localhost:27017/mongodataapp")
+  .then(() => console.log("connection successfull..."))
+  .catch((err) => console.log(err));
+const playListSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  ctype: String,
+  videos: Number,
+  author: String,
+  active: Boolean,
+  date: { type: Date, default: Date.now },
+});
+const PLayList = new mongoose.model("PLayList", playListSchema);
+const createDocument = async () => {
+  try {
+    const reactPlayList = new PLayList({
+        name: "Node Js",
+        ctype: "Back End",
+        videos: 86,
+        author: "Nabeel",
+        active: true,
+      });
+      const mongodbPlayList = new PLayList({
+        name: "MongoDB",
+        ctype: "Database",
+        videos: 86,
+        author: "Nabeel",
+        active: true,
+      });const expressPlayList = new PLayList({
+        name: "Express Js",
+        ctype: "Back End",
+        videos: 86,
+        author: "Nabeel",
+        active: true,
+      });
+      
+        
+    const jsPlayList = new PLayList({
+      name: "Javascript",
+      ctype: "Front End",
+      videos: 186,
+      author: "Nabeel",
+      active: true,
+    });
+    const result = await PLayList.insertMany([jsPlayList,expressPlayList,mongodbPlayList,reactPlayList]);
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+};
+createDocument();
